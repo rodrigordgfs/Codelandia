713cdae914a28edd60e1d1db21537beb8121b764
Rodrigo Viegas Rodrigues <rodrigoviegas@lojaspompeia.com.br>
2021-06-28 02:26:36 -0300

    Request in a Fake API to fetch publication data

---
+++ js/script.js
@@ -0,0 +1,61 @@
+let posts = [];
+
+async function getAPIData() {
+  try {
+    const response = await fetch(
+      "https://60d94dabeec56d00174776a2.mockapi.io/api/v1/post"
+    );
+    return await response.json();
+  } catch (error) {
+    throw new Error(error);
+  }
+}
+
+getAPIData()
+  .then((data) => {
+    const posts = data;
+    for (const post of posts) {
+      let main = document.getElementById("main");
+
+      let description = document.createElement("p");
+      description.appendChild(document.createTextNode(post.description));
+      let main_card_description = document.createElement("div");
+      main_card_description.classList.add("main__card__description");
+      main_card_description.appendChild(description);
+
+      let title = document.createElement("p");
+      title.appendChild(document.createTextNode(post.title));
+      let main_card_title = document.createElement("div");
+      main_card_title.classList.add("main__card__title");
+      main_card_title.appendChild(title);
+
+      let post_date = document.createElement("p");
+      post_date.classList.add("main__card__header_text");
+      let created_at = moment(post.created_at).locale('pt-br').format('ll')
+      post_date.appendChild(document.createTextNode(created_at));
+      let favorite_icon = document.createElement("span");
+      favorite_icon.classList.add("material-icons", "main__card__header_icon");
+      favorite_icon.appendChild(document.createTextNode("favorite_border"));
+      let main_card_header = document.createElement("div");
+      main_card_header.classList.add("main__card__header");
+      main_card_header.appendChild(post_date);
+      main_card_header.appendChild(favorite_icon);
+
+      let main_card = document.createElement("div");
+      main_card.classList.add(
+        "main__card", "animate__animated", "animate__backInLeft"
+      );
+      main_card.appendChild(main_card_header);
+      main_card.appendChild(main_card_title);
+      main_card.appendChild(main_card_description);
+
+      let container = document.createElement("div")
+      container.classList.add("container");
+      container.appendChild(main_card);
+
+      main.appendChild(container);
+    }
+  })
+  .catch((error) => {
+    console.error(error);
+  });
