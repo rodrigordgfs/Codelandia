e87c13b7929fb6b94e9d30ddd7d7a3a16850ec3e
Rodrigo Viegas Rodrigues <rodrigoviegas@lojaspompeia.com.br>
2021-06-28 02:55:14 -0300

    Removed fonts folder

--- js/script.js
+++ js/script.js
@@ -12,53 +12,59 @@ async function getAPIData() {
 }
 
 function buildList() {
-    for (const post of posts) {
-        let main = document.getElementById("main");
-  
-        let description = document.createElement("p");
-        description.appendChild(document.createTextNode(post.description));
-        let main_card_description = document.createElement("div");
-        main_card_description.classList.add("main__card__description");
-        main_card_description.appendChild(description);
-  
-        let title = document.createElement("p");
-        title.appendChild(document.createTextNode(post.title));
-        let main_card_title = document.createElement("div");
-        main_card_title.classList.add("main__card__title");
-        main_card_title.appendChild(title);
-  
-        let post_date = document.createElement("p");
-        post_date.classList.add("main__card__header_text");
-        let created_at = moment(post.created_at).locale('pt-br').format('ll')
-        post_date.appendChild(document.createTextNode(created_at));
-        let favorite_icon = document.createElement("span");
-        favorite_icon.classList.add("material-icons", "main__card__header_icon");
-        favorite_icon.appendChild(document.createTextNode(`${post.favorite ? 'favorite' : 'favorite_border'}`));
-        let main_card_header = document.createElement("div");
-        main_card_header.classList.add("main__card__header");
-        main_card_header.appendChild(post_date);
-        main_card_header.appendChild(favorite_icon);
-  
-        let main_card = document.createElement("div");
-        main_card.classList.add(
-          "main__card", "animate__animated", "animate__backInLeft"
-        );
-        main_card.appendChild(main_card_header);
-        main_card.appendChild(main_card_title);
-        main_card.appendChild(main_card_description);
-  
-        let container = document.createElement("div")
-        container.classList.add("container");
-        container.appendChild(main_card);
-  
-        main.appendChild(container);
-      }
+  for (const post of posts) {
+    let main = document.getElementById("main");
+
+    let description = document.createElement("p");
+    description.appendChild(document.createTextNode(post.description));
+    let main_card_description = document.createElement("div");
+    main_card_description.classList.add("main__card__description");
+    main_card_description.appendChild(description);
+
+    let title = document.createElement("p");
+    title.appendChild(document.createTextNode(post.title));
+    let main_card_title = document.createElement("div");
+    main_card_title.classList.add("main__card__title");
+    main_card_title.appendChild(title);
+
+    let post_date = document.createElement("p");
+    post_date.classList.add("main__card__header_text");
+    let created_at = moment(post.created_at).locale("pt-br").format("ll");
+    post_date.appendChild(document.createTextNode(created_at));
+    let favorite_icon = document.createElement("span");
+    favorite_icon.classList.add("material-icons", "main__card__header_icon");
+    favorite_icon.appendChild(
+      document.createTextNode(
+        `${post.favorite ? "favorite" : "favorite_border"}`
+      )
+    );
+    let main_card_header = document.createElement("div");
+    main_card_header.classList.add("main__card__header");
+    main_card_header.appendChild(post_date);
+    main_card_header.appendChild(favorite_icon);
+
+    let main_card = document.createElement("div");
+    main_card.classList.add(
+      "main__card",
+      "animate__animated",
+      "animate__backInLeft"
+    );
+    main_card.appendChild(main_card_header);
+    main_card.appendChild(main_card_title);
+    main_card.appendChild(main_card_description);
+
+    let container = document.createElement("div");
+    container.classList.add("container");
+    container.appendChild(main_card);
+
+    main.appendChild(container);
+  }
 }
 
 getAPIData()
   .then((data) => {
     posts = data;
-    buildList()
+    buildList();
   })
   .catch((error) => {
     console.error(error);
